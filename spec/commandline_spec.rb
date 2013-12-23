e2edc93e4be441802c795bcc1bee85d9d12c377d
Matthias Guenther <matthias@wikimatze.de>
2013-12-23 18:55:23 +0100

    Add pending specs

--- spec/commandline_spec.rb
+++ spec/commandline_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper.rb'
 
 describe Commandline do
+  context "questions" do
+    xit "asks for the name of the author"
+    xit "asks for the email adress of the author"
+    xit "asks after the module name"
+  end
 
+  context "confirmation" do
+    xit "new module creation message"
+    xit "new module creation"
+    xit "validation of input information"
+  end
+
+  context "output" do
+    context "files" do
+      xit "creates files directory"
+      xit "creates templates directory"
+      xit "creates manifests directory"
+      context "manifests" do
+        xit "creates init.pp file"
+        xit "creates package.pp file"
+      end
+    end
+  end
+
+  context "generation" do
+    # tbd
+  end
 end
