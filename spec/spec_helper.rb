03b304c44adb1d5841353dc4e9667aa034e39fb1
Matthias Guenther <matthias@wikimatze.de>
2013-12-25 11:35:27 +0100

    Include minitest capture_io to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/../commandline.rb")
+require 'minitest/unit'
 
 RSpec.configure do |conf|
   conf.color_enabled = true
+  conf.include Minitest::Assertions
 end
